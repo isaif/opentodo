89a013f13a7491a59fc7f419e71efbc9bcd850f6
isaif <saif935775@gmail.com>
2022-09-12 20:39:07 +0530

    fix: overflowing content in TodoItem span

--- utils/data.ts
+++ utils/data.ts
@@ -4,6 +4,11 @@ const data: Todo[] = [
   { id: 1, name: "first todo", completed: false },
   { id: 2, name: "second todo", completed: true },
   { id: 3, name: "third todo", completed: false },
+  {
+    id: 4,
+    name: "the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog",
+    completed: false,
+  },
 ];
 
 export default data;
