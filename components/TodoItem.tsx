4b114a130daa4cddaa5012079b72818f4df4154a
isaif <saif935775@gmail.com>
2022-09-12 20:35:20 +0530

    feat: add checkbox to TodoItem
    - move TodoItem styling to TodoItem.module.css

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,9 +1,20 @@
 import { Todo } from "../utils/interfaces";
 
+import styles from "../styles/TodoItem.module.css";
+
 interface Props {
   todo: Todo;
 }
 
 export const TodoItem = ({ todo }: Props) => {
-  return <li>{todo.name}</li>;
+  return (
+    <li className={styles.todoItem}>
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        className={styles.checkbox}
+      />
+      <span>{todo.name}</span>
+    </li>
+  );
 };
--- utils/data.ts
+++ utils/data.ts
@@ -2,7 +2,7 @@ import { Todo } from "./interfaces";
 
 const data: Todo[] = [
   { id: 1, name: "first todo", completed: false },
-  { id: 2, name: "second todo", completed: false },
+  { id: 2, name: "second todo", completed: true },
   { id: 3, name: "third todo", completed: false },
 ];
 
